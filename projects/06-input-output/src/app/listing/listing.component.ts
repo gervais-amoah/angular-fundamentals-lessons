160fa89ce14f2bd3fcc5e50454e6b1132aa125aa
gervais <gervaisamoah@yahoo.com>
2024-05-07 16:01:11 +0000

    project 6

--- projects/06-input-output/src/app/listing/listing.component.ts
+++ projects/06-input-output/src/app/listing/listing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Car } from '../car';
 
@@ -6,7 +6,42 @@ import { Car } from '../car';
   selector: 'app-listing',
   standalone: true,
   imports: [CommonModule],
-  template: ` <!-- listing markup goes here --> `,
+  template: `
+    <article class="listing">
+      <div class="image-parent">
+        <img class="product-image" src="https://placehold.co/100x100" />
+      </div>
+      <section class="details">
+        <p class="title">{{ car.model }} {{ car.make }}</p>
+        <hr />
+        <p class="detail">
+          <span>Year</span>
+          <span>{{ car.year }}</span>
+        </p>
+        <div class="detail">
+          <span>Transmission</span>
+          <span>{{ car.transmission }}</span>
+        </div>
+        <p class="detail">
+          <span>Mileage</span>
+          <span>{{ car.miles }}</span>
+        </p>
+        <p class="detail">
+          <span>Price</span>
+          <span>{{ car.price }}</span>
+        </p>
+      </section>
+    </article>
+    <button (click)="handleCarSave()">Save Car</button>
+  `,
   styles: ``,
 })
-export class ListingComponent {}
+export class ListingComponent {
+  @Input({ required: true }) car!: Car;
+
+  @Output() carSaved = new EventEmitter<Car>();
+
+  handleCarSave() {
+    this.carSaved.emit(this.car);
+  }
+}
