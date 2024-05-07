dcc30364cd209354e7a82250e69dc768a60cd7df
gervais <gervaisamoah@yahoo.com>
2024-05-07 16:00:07 +0000

    project 4

--- projects/04-control-flow-if/src/app/app.component.ts
+++ projects/04-control-flow-if/src/app/app.component.ts
@@ -11,15 +11,19 @@ import { AccountInfo } from './account-info';
       <section class="membership-info">
         <p>
           <!-- name -->
-          {{ '' }}
+          {{ account.name }}
         </p>
-        <p>Valid Thru: {{ '' }}</p>
-        <p>CVV: {{ '' }}</p>
+        <p>Valid Thru: {{ account.validThru }}</p>
+        <p>CVV: {{ account.CVV }}</p>
         <p>
           <!-- membership status -->
-          <span class="badge gold">Gold</span>
-          <span class="badge platinum">Platinum</span>
+          @switch (account.membershipStatus) { @case ('silver') {
           <span class="badge silver">Silver</span>
+          } @case ('platinum') {
+          <span class="badge platinum">Platinum</span>
+          } @case ('gold') {
+          <span class="badge gold">Gold</span>
+          } }
         </p>
       </section>
     </article>
